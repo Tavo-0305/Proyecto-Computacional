47bde11987c60785dbf89dc1183f392c3467b473
ManzaLord <fxymanzar3@gmail.com>
2024-11-24 10:19:33 -0600

    Implementacion para multiples discos

--- main/simulation/main.py
+++ main/simulation/main.py
@@ -1,13 +1,55 @@
 #!/usr/bin/env python3
 
+import numpy as np
 from classes import Disco, Escenario
 #Creamos los objetos de tipo disco
-disco1 = Disco(0.2,0.2,'red')
-disco2 = Disco(0.8,0.8,'blue')
-disco3 = Disco(0.8,0.2,'green')
-disco4 = Disco(0.2,0.8,'orange')
+
+espacioHorizontal = 1.0
+espacioVertical = 1.0
+cantidadDiscos = 50
+
+largoMatriz = 1
+while True:
+    if largoMatriz * largoMatriz < cantidadDiscos:
+        largoMatriz +=1
+    else:
+        break
+
+distanciaHorizontal = espacioHorizontal / (largoMatriz + 1)
+distanciaVertical = espacioVertical / (largoMatriz + 1)
+posiciones = np.zeros([largoMatriz * largoMatriz,2])
+posiciones[0] = [distanciaHorizontal,distanciaVertical]
+
+contador = 1
+for i in range(1,largoMatriz * largoMatriz):
+    if round(posiciones[i-1][0] + distanciaHorizontal,2) >= espacioHorizontal:
+        posiciones[i][0] = distanciaHorizontal
+        posiciones[i][1] = distanciaVertical + posiciones[i-1][1]
+    else:
+        posiciones[i][0] = distanciaVertical + posiciones[i-1][0]
+        posiciones[i][1] = posiciones[i-1][1]
+
+indicesAleatorios = np.random.choice(largoMatriz * largoMatriz,cantidadDiscos,False)
+posiciones = posiciones[indicesAleatorios]
+
+discos = [0] * cantidadDiscos
+
+for i in range(cantidadDiscos):
+    if i % 4 == 0:
+        color = 'blue'
+    elif i % 4 == 1:
+        color = 'red'
+    elif i % 4 == 2:
+        color = 'green'
+    else:
+        color = 'orange'
+    
+    radio = np.random.uniform(0.01,0.05)
+
+    discos[i] = Disco(posiciones[i][0],posiciones[i][1],color,radio)
+
 #Objeto de tipo animación
-simulacion = Escenario([disco1,disco2,disco3,disco4]) #Recuerde que la dimensión es unitaria
+simulacion = Escenario(discos,0.005) #Recuerde que la dimensión es unitaria
 #Corrermos la simulacion
 simulacion.runSimulation()
 #Además debemos implementar un script para crear un archivo .csv con los datos de los discos
